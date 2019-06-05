46d3f7870fa48747df7d44c5fc6b17f79968853c
hakan çelik <hakancelik96@outlook.com>
2019-06-05 13:16:33 +0300

    added template tags| counts and is_vote

---
+++ django_vote_system/templatetags/vote.py
@@ -0,0 +1,36 @@
+# django
+from django import template
+from django.contrib.contenttypes.models import ContentType
+
+# models
+from ..models import VoteCount, Vote
+
+register = template.Library()
+
+def get_content_type(model):
+    content_type_obj = ContentType.objects.get_for_model(model)
+    content_type = ContentType.objects.get(
+        app_label=content_type_obj.app_label, 
+        model=content_type_obj.model
+    )
+    return content_type
+
+def common_count(model, id):
+    return VoteCount.objects.get(content_type=get_content_type(model), object_id=id)
+
+register.filter
+def upvote_count(model, id):
+    return common_count(model, id).upvote_count
+
+register.filter
+def downvote_count(model, id):
+    return common_count(model, id).downvote_count
+
+@register.simple_tag
+def is_vote(user, model, id):
+    obj = Vote.objects.filter(user=user, content_type=get_content_type(model), object_id=id)
+    if obj.filter(status=True).exists():
+        return True
+    elif obj.filter(status=False).exists():
+        return False
+    return None
